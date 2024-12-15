3467d14e412f9e54e635a34d82cdb512f44d9f57
Richard Vodden <richard@humanisingautonomy.com>
2024-12-15 22:30:20 +0000

    Day 15 part 2 WIP

--- src/days/15/Puzzle.ts
+++ src/days/15/Puzzle.ts
@@ -1,114 +1,275 @@
 type Point = [number, number];
 
-enum Contents {
-    EMPTY,
-    BOX,
-    WALL,
-    ROBOT
-}
-
 const up = ([x, y]: Point): Point => [x, y - 1];
 const down = ([x, y]: Point): Point => [x, y + 1];
 const left = ([x, y]: Point): Point => [x - 1, y];
 const right = ([x, y]: Point): Point => [x + 1, y];
 
-const neighbours: { ([x, y]: Point): Point }[] = [
-    up,
-    down,
-    left,
-    right
-];
+const equal = (lhs: Point, rhs: Point): boolean => lhs[0] === rhs[0] && lhs[1] === rhs[1];
 
-const parseInput = (input: string) : { map: Contents[][], directions: { ([x, y]: Point): Point }[], position: Point} => {
+const parseInput = (
+    input: string,
+): { boxes: Point[]; walls: Point[]; directions: { ([x, y]: Point): Point }[]; position: Point } => {
     const walls: Point[] = [];
     const boxes: Point[] = [];
     let postition: Point;
 
-    const [mapText, directionText] = input.split("\n\n");
+    const [mapText, directionText] = input.split('\n\n');
 
-    const maxX = mapText.indexOf("\n");
+    const maxX = mapText.indexOf('\n');
     const maxY = (mapText.length + 1) / maxX - 1;
 
     let position: Point;
-    const map = mapText.split("\n").map((line, y) =>
+    const map = mapText.split('\n').map((line, y) =>
         [...line].map((char, x) => {
-            switch(char) {
-                case "#":
-                    return Contents.WALL;
-                case ".":
-                    return Contents.EMPTY;
-                case "O":
-                    return Contents.BOX;
-                case "@":
-                    position = [x,y];
-                    return Contents.ROBOT;
+            switch (char) {
+                case '#':
+                    walls.push([x, y]);
+                    break;
+                case 'O':
+                    boxes.push([x, y]);
+                    break;
+                case '@':
+                    position = [x, y];
+                    break;
             }
-        })
-    )
+        }),
+    );
 
-    const directions = [...directionText.replaceAll("\n", "")].map(char => {
-        switch(char) {
-            case "^": 
+    const directions = [...directionText.replaceAll('\n', '')].map((char) => {
+        switch (char) {
+            case '^':
                 return up;
-            case "v": 
+            case 'v':
                 return down;
-            case "<": 
+            case '<':
                 return left;
-            case ">": 
+            case '>':
                 return right;
+            default:
+                console.error('epic fail');
+                process.exit(-1);
         }
-    })
-    return { map, directions, position };
-}
+    });
+    return { boxes, walls, directions, position };
+};
 
-const move = ([x, y]: Point, direction: {([x, y]: Point): Point}, map: Contents[][]): [Contents[][], Point] => {
-    const [newX, newY] = direction([x, y]);
-    const destinationContents = map[newY][newX];
-    switch(destinationContents) {
-        case Contents.BOX:
-            // try and move the box out of the way
-            const [newMap, _] = move([newX, newY], direction, map);
+const isIn = (p: Point, collection: Point[]): boolean => collection.find((item) => equal(item, p)) != undefined;
 
-            // if it moved
-            if (newMap[newY][newX] === Contents.EMPTY) {
-                // use the space
-                newMap[newY][newX] = map[y][x];
-                newMap[y][x] = Contents.EMPTY;
-                return [newMap, [newX, newY]]
-            }
-
-            return [map, [x, y]];
-        case Contents.EMPTY:
-            map[newY][newX] = map[y][x];
-            map[y][x] = Contents.EMPTY;
-            return [map, [newX, newY]];
-        case Contents.WALL:
-            return [map, [x, y]];
+const moveBox = (
+    position: Point,
+    direction: { ([x, y]: Point): Point },
+    boxes: Point[],
+    walls: Point[],
+): Point[] | undefined => {
+    const newPosition = direction(position);
+    if (isIn(newPosition, walls)) {
+        // is there a wall in the way?
+        return;
     }
-    return [map, [x, y]];
-}
 
+    if (isIn(newPosition, boxes)) {
+        // is there a box in the way?
+        const newBoxes = moveBox(newPosition, direction, boxes, walls); // try and shift it
+        if (newBoxes === undefined) {
+            // box didn't move
+            return; // return original state
+        }
+        boxes = newBoxes;
+    }
+
+    // move the box to the new place.
+    boxes.splice(
+        boxes.findIndex((box) => equal(box, position)),
+        1,
+    );
+    boxes.push(newPosition);
+
+    return boxes;
+};
+
+const move = (
+    position: Point,
+    direction: { ([x, y]: Point): Point },
+    boxes: Point[],
+    walls: Point[],
+): { position: Point; boxes: Point[] } => {
+    const newPosition = direction(position);
+
+    if (isIn(newPosition, walls)) {
+        // is there  wall in the way?
+        return { position, boxes }; // bail out
+    }
+
+    if (isIn(newPosition, boxes)) {
+        // is there a box in the way?
+        const newBoxes = moveBox(newPosition, direction, boxes, walls); // try and move it
+
+        if (newBoxes === undefined) {
+            // if box didn't move
+            return { position, boxes }; // bail out
+        }
+        boxes = newBoxes;
+    }
+
+    return { position: newPosition, boxes };
+};
 
 const part1 = (input: string) => {
-    const {map, directions, position} = parseInput(input);
-    const [newMap, newPosition] =  directions.reduce(([map, position], direction) => move(position, direction, map), [map, position])
-    // const [newMap, newPosition] = move(position, up, map);
-   
-    console.log(newMap);
+    const { boxes, walls, directions, position } = parseInput(input);
+    console.log(boxes);
 
-    return newMap.map((row, y) => row.map(
-            (content, x) => content === Contents.BOX ? (x + 100 * y) : 0)
-            .reduce((lhs, rhs) => lhs + rhs, 0)
-        )
-        .reduce((lhs, rhs) => lhs + rhs, 0);
+    const { boxes: newBoxes } = directions.reduce(
+        ({ position, boxes }, direction) => move(position, direction, boxes, walls),
+        { position, boxes },
+    );
+    console.log(newBoxes);
+    return boxes.map(([x, y]) => y * 100 + x).reduce((lhs, rhs) => lhs + rhs, 0);
 };
 
 const expectedFirstSolution = 10092;
 
-const part2 = (input: string) => {
-  return 'part 2';
+const equalBoxes = (lhs: [Point, Point], rhs: [Point, Point]): boolean => equal(lhs[0], rhs[0]) && equal(lhs[1], rhs[1]);
+const containedInBox = ( lhs: Point, rhs: [Point, Point]): boolean => equal(lhs, rhs[0]) || equal(lhs, rhs[1]);
+
+const parseInputP2 = (
+    input: string,
+): { boxes: [Point, Point][]; walls: Point[]; directions: { ([x, y]: Point): Point }[]; position: Point } => {
+    const walls: Point[] = [];
+    const boxes: [Point, Point][] = [];
+    let postition: Point;
+
+    const [mapText, directionText] = input.split('\n\n');
+
+    const maxX = mapText.indexOf('\n');
+    const maxY = (mapText.length + 1) / maxX - 1;
+
+    let position: Point;
+    const map = mapText.split('\n').map((line, y) =>
+        [...line].map((char, x) => {
+            switch (char) {
+                case '#':
+                    walls.push([2 * x, y]);
+                    walls.push([2 * x + 1, y]);
+                    break;
+                case 'O':
+                    boxes.push([
+                        [2 * x, y],
+                        [2 * x + 1, y],
+                    ]);
+                    break;
+                case '@':
+                    position = [x, y];
+                    break;
+            }
+        }),
+    );
+
+    const directions = [...directionText.replaceAll('\n', '')].map((char) => {
+        switch (char) {
+            case '^':
+                return up;
+            case 'v':
+                return down;
+            case '<':
+                return left;
+            case '>':
+                return right;
+            default:
+                console.error('epic fail');
+                process.exit(-1);
+        }
+    });
+    return { boxes, walls, directions, position };
 };
 
-const expectedSecondSolution = 'part 2';
+const moveBoxP2 = (
+    box: [Point, Point],
+    direction: { ([x, y]: Point): Point },
+    boxes: [Point, Point][],
+    walls: Point[],
+): [Point, Point][] | undefined => {
+    const newBox = box.map(direction) as [Point, Point];
+    if (newBox.some(point => isIn(point, walls))) {
+        // is there a wall in the way?
+        return;
+    }
+
+    // are there any boxes in the way?
+    if (newBox.some(point => isIn(point, boxes.flat()))) {
+        // There's at least one box in the way, so find the first in the way box, and move it
+        let idx = boxes.findIndex(b => b.some(p => containedInBox(p, newBox)));
+        let newBoxes = moveBoxP2(boxes[idx], direction, boxes, walls); // try and shift it
+        if (newBoxes === undefined) {
+            // box didn't move
+            return; // return original state
+        }
+
+        // find the next box
+        idx = newBoxes.findIndex((b, i) => i > idx && b.some(p => containedInBox(p, newBox)));
+        if(idx != -1) { // if there is one
+            newBoxes = moveBoxP2(newBoxes[idx], direction, newBoxes, walls); // try and shift it
+            if (newBoxes === undefined) {
+                // box didn't move
+                return; // bail
+            }
+        }
+        
+        boxes = newBoxes;
+    }
+
+    // move the box to the new place.
+    boxes.splice(
+        boxes.findIndex((b) => equalBoxes(b, box)),
+        1,
+    );
+    boxes.push(newBox);
+
+    return boxes;
+};
+
+const moveP2 = (
+    position: Point,
+    direction: { ([x, y]: Point): Point },
+    boxes: [Point, Point][],
+    walls: Point[],
+): { position: Point; boxes: [Point, Point][] } => {
+    const newPosition = direction(position);
+
+    if (isIn(newPosition, walls)) {
+        // is there  wall in the way?
+        return { position, boxes }; // bail out
+    }
+
+    // are there any boxes in the way?
+    if (isIn(newPosition, boxes.flat())) {
+        // is there one box in the way?
+        let idx = boxes.findIndex(box => containedInBox(newPosition, box));
+        let newBoxes = moveBoxP2(boxes[idx], direction, boxes, walls); // try and move it
+
+        if (newBoxes === undefined) {
+            // if box didn't move
+            return { position, boxes }; // bail out
+        }
+
+        boxes = newBoxes;
+    }
+
+    return { position: newPosition, boxes };
+};
+
+const part2 = (input: string) => {
+    const { boxes, walls, directions, position } = parseInputP2(input);
+    console.log(boxes);
+
+    const { boxes: newBoxes } = directions.reduce(
+        ({ position, boxes }, direction) => moveP2(position, direction, boxes, walls),
+        { position, boxes },
+    );
+    console.log(newBoxes);
+    return boxes.map(([[x, y], _]) => y * 100 + x).reduce((lhs, rhs) => lhs + rhs, 0);
+};
+
+const expectedSecondSolution = 9021;
 
 export { part1, expectedFirstSolution, part2, expectedSecondSolution };
