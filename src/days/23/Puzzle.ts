99afecbca063fbf5c0ec7b350d467215d81548c4
Richard Vodden <richard@humanisingautonomy.com>
2024-12-25 10:06:24 +0000

    Completed Day 23

--- src/days/23/Puzzle.ts
+++ src/days/23/Puzzle.ts
@@ -1,97 +1,92 @@
-const addVertex = (vertices: Map<string, Set<string>>, source: string, destination: string) => {
-    if(!vertices.has(source)) vertices.set(source, new Set<string>());
-    vertices.get(source)!.add(destination)
+type Graph = Map<string, Set<string>>;
+
+const addVertex = (vertices: Graph, source: string, destination: string) => {
+    if (!vertices.has(source)) vertices.set(source, new Set<string>());
+    vertices.get(source)!.add(destination);
 
     return vertices;
-}
+};
 
-const equal = <T>(lhs: Set<T>, rhs: Set<T>) => (lhs.size === rhs.size) && lhs.values().every(elem => rhs.has(elem));
+const equal = <T>(lhs: Set<T>, rhs: Set<T>) => lhs.size === rhs.size && lhs.values().every((elem) => rhs.has(elem));
 
-const parseInput = (input: string): { nodes: Set<string>, vertices: Map<string, Set<string>> } => {
-    const nodes = new Set<string>();
+const parseInput = (input: string): Graph => {
     const vertices = new Map<string, Set<string>>();
 
-    input.split("\n").filter(x => x != "").map(line => line.split("-"))
+    input
+        .split('\n')
+        .filter((x) => x != '')
+        .map((line) => line.split('-'))
         .forEach(([source, destination]) => {
-            nodes.add(source)
-            nodes.add(destination)
-
             addVertex(vertices, source, destination);
             addVertex(vertices, destination, source);
-        })
+        });
 
-    return { nodes, vertices };
-}
+    return vertices;
+};
 
 const part1 = (input: string): number => {
-    const {nodes, vertices} = parseInput(input);
+    const vertices = parseInput(input);
 
-    const potentialNodes = new Set(nodes.values().filter(node => node[0] == "t"));
+    const potentialNodes = new Set(vertices.keys().filter((node) => node[0] == 't'));
     const threeCliques: Set<string>[] = [];
 
-    potentialNodes.forEach(source => {
-        vertices.get(source)!.forEach(destination => {
-            vertices.get(destination)!.forEach(node => {
-                if( vertices.get(node)!.has(source) ) {
+    potentialNodes.forEach((source) => {
+        vertices.get(source)!.forEach((destination) => {
+            vertices.get(destination)!.forEach((node) => {
+                if (vertices.get(node)!.has(source)) {
                     const clique = new Set([source, destination, node]);
-                    if(threeCliques.find(lhs => equal(lhs, clique)) === undefined) {
+                    if (threeCliques.find((lhs) => equal(lhs, clique)) === undefined) {
                         threeCliques.push(clique);
                     }
                 }
             });
-        })
-    })
+        });
+    });
 
-    return threeCliques.length
+    return threeCliques.length;
 };
 
 const expectedFirstSolution = 7;
 
-const maxClique = (nodes: Set<string>, vertices: Map<string, Set<string>>) => {
-    let max = 0;
-    let c = new Map<string, number>(nodes.values().map(node => [node, 0]));
-    
-    const clique = (nodes: Set<string>, size: number): number[] | undefined => {
-        console.log(nodes, size);
-        if (nodes.size == 0) {
-            if (size > max) {
-                max = size;
-                return [];
-            }
-            return
-        }
+const isClique = (vertices: Graph, nodes: Set<string>) =>
+    nodes.values().every((node1) => nodes.values().every((node2) => node1 == node2 || vertices.get(node2)?.has(node1)));
 
-        nodes = new Set(nodes);
-        while(nodes.size > 0) {
-            if (nodes.size + size < max ) return; // don't have enough nodes to break the record
-    
-            const node = nodes.values().next().value;
-            nodes.delete(node);
+const copyGraph = (vertices: Graph): Graph =>
+    new Map([...vertices.entries()].map(([node, adjacencies]) => [node, new Set([...adjacencies])]));
 
-            if (nodes.size + c.get(node)! + size <= max) return;
+// const nextCliques = (nodes: Set<string>, vertices: Graph): IteratorObject<Set<string>> =>
+//     new Set(vertices.keys()).difference(nodes).values().map(node => new Set([node, ...nodes])).filter(subGraph => isClique(subGraph, vertices))
 
-            const res = clique(nodes.intersection(vertices.get(node)!), size + 1)
-            if (res !== undefined) return [node, ...res]
-        }
-        return undefined;
+const remove = (node: string, vertices: Graph): Graph => {
+    vertices = copyGraph(vertices);
+    vertices.delete(node);
+    vertices.forEach((adjacencies) => adjacencies.delete(node));
+    return vertices;
+};
+
+
+//                                        R                      P                    X
+const bronKerbosch = (vertices: Graph, clique: Set<string>, toVisit: Set<string>, excluded: Set<string> ): Set<string>[] => {
+    if ( excluded.size === 0 && toVisit.size === 0 ) return [clique];
+
+    const cliques = []
+
+    for( const node of toVisit ) {
+        const neighbours = vertices.get(node)!;
+        cliques.push(...bronKerbosch(vertices, new Set([node, ...clique]), toVisit.intersection(neighbours), excluded.intersection(neighbours)));
+        toVisit.delete(node);
+        excluded.add(node);
     }
 
-    const cliques = nodes.values().map(node => {
-        const res = clique(nodes.intersection(vertices.get(node)!), 1);
-        c.set(node, max);
-        nodes.delete(node);
-        return res === undefined ? undefined : [node, ...res!];
-    }).filter(x => x != undefined).toArray();
-
-    console.log(c);
-
-    return cliques
-}
+    return cliques;
+};
 
 const part2 = (input: string) => {
-    const {nodes, vertices} = parseInput(input);
+    const vertices = parseInput(input);
 
-    return maxClique(nodes, vertices);
+    const cliques = bronKerbosch(vertices, new Set(), new Set(vertices.keys()), new Set());
+
+    return [...cliques.reduce((max, set) => max.size <= set.size ? set : max)].sort().join(",");
 };
 
 const expectedSecondSolution = 'co,de,ka,ta';
