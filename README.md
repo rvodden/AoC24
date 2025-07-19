62b2444018b813bd847b4b9aac81a515124b9a15
Richard Vodden <richard@humanisingautonomy.com>
2025-07-19 09:40:51 +0000

    WIP

---
+++ README.md
@@ -0,0 +1,18 @@
+# AOC 2024
+
+To run a day against real input execute:
+
+```
+> npm run dev 06
+```
+
+Note that for days <10 you will need to include a leading zero.
+
+
+Run tests of a day (i.e. against the example input) by running:
+
+```
+> npx vitest -t 'Tests day 06' --hideSkippedTests
+```
+
+again with leading zeros
--- src/days/06/Puzzle.ts
+++ src/days/06/Puzzle.ts
@@ -1,12 +1,24 @@
+import { PipelineTransform } from "stream";
+import { N } from "vitest/dist/chunks/reporters.D7Jzd9GS.js";
+
 type Point = [number, number];
 
 const sum = ([x1, y1]: Point, [x2, y2]: Point): Point => [x1 + x2, y1 + y2];
+const subtract = ([x1, y1]: Point, [x2, y2]: Point): Point => [x1 - x2, y1 - y2];
 const areEqual = ([x1, y1]: Point, [x2, y2]: Point): boolean => x1 === x2 && y1 === y2;
 
-const Up = [0, -1] as Point;
-const Down = [0, 1] as Point;
-const Left = [-1, 0] as Point;
-const Right = [1, 0] as Point;
+const North = [0, -1] as Point;
+const South = [0, 1] as Point;
+const East = [-1, 0] as Point;
+const West = [1, 0] as Point;
+
+/**
+ * @returns true if target is due east of reference 
+ */
+const isEastOf = (target: Point, reference: Point) => reference[1] === target[1] && target[0] > reference[0]
+const isWestOf = (target: Point, reference: Point) => reference[1] === target[1] && target[0] < reference[0]
+const isSouthOf = (target: Point, reference: Point) => reference[0] === target[0] && target[1] > reference[1]
+const isNorthOf = (target: Point, reference: Point) => reference[0] === target[0] && target[1] < reference[1]
 
 const rotateClockwise = ([x, y]: Point): Point => [-y, x];
 const rotateAnticlockwise = ([x, y]: Point): Point => [y, -x];
@@ -36,100 +48,193 @@ const parseInput = (input: string): { obstructions: Point[]; position: Point; bo
     return { obstructions, position, bounds };
 };
 
+
+/**
+ * @returns true if `position` is within `bounds`; false otherwise
+ */
 const inBounds = (position: Point, bounds: Point): boolean => {
     return position[0] >= 0 && position[0] < bounds[0] && position[1] >= 0 && position[1] < bounds[1];
 };
 
 const tracePath = (obstructions: Point[], start_position: Point, bounds: Point) => {
-    let heading = Up;
+    let heading = North;
     let position = start_position;
     let positions: [Point, Point][] = []; // position, heading
-    let collisions: [Point, Point, number][] = []; // heading, obstruction, index
-    let index = 0
     do {
         if (!positions.find((p) => areEqual(p[0], position))) positions.push([position, heading]);
 
         let new_position = sum(position, heading);
         let obstruction;
-        if ((obstruction = obstructions.find((p) => areEqual(p, new_position))) !== undefined) {
-            collisions.push([heading, obstruction, index]);
+        while ((obstruction = obstructions.find((p) => areEqual(p, new_position))) !== undefined) {
             heading = rotateClockwise(heading);
             new_position = sum(position, heading);
         }
         position = new_position;
-        index++;
     } while (inBounds(position, bounds));
 
-    return { positions, collisions };
-};
-
-const tracePathP2 = (obstructions: Point[], start_position: Point, bounds: Point) => {
-    let heading = Up;
-    let position = start_position;
-    let positions: [Point, Point][] = []; // position, heading
-    let collisions: [Point, Point, number][] = []; // heading, obstruction, index
-    let index = 0
-    do {
-        positions.push([position, heading]);
-
-        let new_position = sum(position, heading);
-        let obstruction;
-        if ((obstruction = obstructions.find((p) => areEqual(p, new_position))) !== undefined) {
-            collisions.push([heading, obstruction, index]);
-            heading = rotateClockwise(heading);
-            index++;
-            continue;
-        }
-        position = new_position;
-        index++;
-    } while (inBounds(position, bounds));
-
-    return { positions, collisions };
+    return positions;
 };
 
 const part1 = (input: string) => {
     const { obstructions, position: start_position, bounds } = parseInput(input);
-    const { positions, collisions } = tracePath(obstructions, start_position, bounds);
+    const positions = tracePath(obstructions, start_position, bounds);
 
     return positions.length;
 };
 
 const expectedFirstSolution = 41;
 
-const part2 = (input: string) => {
-    const { obstructions, position: start_position, bounds } = parseInput(input);
-    const { positions, collisions } = tracePathP2(obstructions, start_position, bounds);
-    const potential_positions: [Point, Point][] = []; // position & heading
+const nextPosition = (position: Point, heading: Point, obstructions: Point[]): { position: Point, heading: Point } | undefined => {
+    let filter : { (target: Point) : Boolean};
+    let reduction : { (accumulator: Point, currentValue: Point) : Point }
+    if(areEqual(heading, North)) {
+        filter = (p) => isNorthOf(p, position);
+        reduction = (a, p) => a[0] < p[0] ? a : p;
+    }
+    else if (areEqual(heading, South)) {
+        filter = (p) => isSouthOf(p, position);
+        reduction = (a, p) => a[0] > p[0] ? a : p;
+    }
+    else if (areEqual(heading, East)) {
+        filter = (p) => isEastOf(p, position);
+        reduction = (a, p) => a[1] < p[1] ? a : p;
+    }
+    else if (areEqual(heading, West)) {
+        filter = (p) => isWestOf(p, position);
+        reduction = (a, p) => a[1] > p[1] ? a : p;
+    }
+    
+    const nextObstructions = obstructions.filter(filter)
+    if(nextObstructions.length === 0) {
+        return
+    }
+    const nextObstruction = nextObstructions.reduce(reduction);
+    return {position: subtract(nextObstruction, heading), heading: rotateClockwise(heading)};
+}
 
-    collisions.forEach(([collision_heading, obstruction, index]) => {
+const pointsBetween = (start: Point, end: Point) : Point[] => {
+    if (start[0] === end[0]) {
+        let [s,e] = [start[1], end[1]].sort();
+        return [...Array(e - s)].map(x => [start[0], x + s + 1])
+    }
 
-        let check: (position: Point, path_heading: Point) => boolean;
-        const needed_path_heading = rotateAnticlockwise(collision_heading) as Point;
+    if (start[1] == end[1]) {
+        let [s,e] = [start[0], end[0]].sort();
+        return [...Array(e - s)].map(x => [x + s + 1, start[1]])
+    }
 
-        if (areEqual(collision_heading, Up)) { // collision heading upwards
-            check = ( position, path_heading ) =>
-                areEqual(path_heading,needed_path_heading) && position[0] === obstruction[0] && position[1] > obstruction[1];
-        } else if (areEqual(collision_heading, Down)) {
-            check = ( position, path_heading ) =>
-                areEqual(path_heading,needed_path_heading) && position[0] === obstruction[0] && position[1] < obstruction[1];
-        } else if (areEqual(collision_heading, Left)) {
-            check = ( position, path_heading ) =>
-                areEqual(path_heading,needed_path_heading) && position[0] > obstruction[0] && position[1] === obstruction[1];
-        } else if (areEqual(collision_heading, Right)) {
-            check = ( position, path_heading ) =>
-                areEqual(path_heading,needed_path_heading) && position[0] < obstruction[0] && position[1] === obstruction[1];
+    throw new Error("Points are not on the same row or collumn");
+}
+
+/**
+ * Follow the path of the security guard from start position. When an
+ * obstruction is encontered, rotate clockwise, then continue.
+ * 
+ * This version of the function keeps track of all the colisions and the 
+ * heading at the point the collision took place.
+ * 
+ * @param obstructions   An array of obstructions found on the grid
+ * @param startPosition The starting position
+ * @param bounds         The bounds of the grid
+ * @returns a list of the positions the guard has been in, and a list of the collisions
+ */
+const tracePathP2 = (obstructions: Point[], bounds: Point, startPosition: Point) => {
+    let heading = North;
+    let position = startPosition;
+    let positions: [Point, Point][] = [];
+
+    do {
+        // if we've been here before, throw an exception
+        // TODO: we need to beef up loop detection so that it looks for lines crossing
+        if(positions.find(([oldPosition, oldHeading]) => areEqual(position, oldPosition) && areEqual(heading, oldHeading)) !== undefined) {
+            throw new Error("This is a loop!");
         }
 
-        positions.slice(index).forEach(([position, path_heading]) => {
-            if (check(position, path_heading)) {
-                potential_positions.push([position, path_heading]);
-            }
-        });
-    });
+        positions.push([position, heading]);
+        let {position: newPosition, heading: newHeading } = nextPosition(position, heading, obstructions)!;
+
+        if(!newPosition) break;
+
+        position = newPosition;
+        heading = newHeading;
+    } while (true);
+
+    return { positions };
+};
+
+/**
+ * Part 2
+ *
+ * For every position on the guards walk, see if adding an obstacle in front
+ * would cause the guard to return to a position already visited
+ * 
+ * @param input 
+ * @returns the answer
+ */
+const part2 = (input: string) => {
+    const { obstructions, position: startPosition, bounds } = parseInput(input);
+    const positions = tracePath(obstructions, bounds, startPosition);
+    const newObstaclePositions: [Point, Point][] = []; // position & heading
     
-    return potential_positions.length;
+    console.log(isNorthOf([4,0],[4,6]))
+
+    let np : {position: Point, heading: Point} | undefined = {position: startPosition, heading: North};
+    console.log(np);
+    do {
+        np = nextPosition(np.position, np.heading, obstructions);
+        console.log(np);
+    } 
+    while(np);
+
+    // positions.forEach(([position, heading]) => {
+    //     const obstaclePosition = sum(position, heading);
+
+    //     if(areEqual(obstaclePosition, startPosition)) return;
+
+    //     try {
+    //         tracePathP2([...obstructions, obstaclePosition], bounds, startPosition);
+    //     } catch (error) {
+    //         newObstaclePositions.push([obstaclePosition, heading])
+    //     }
+
+    // });
+
+    // newObstaclePositions.forEach(([position, ]) => console.log(`(${position[0]},${position[1]})`));
+    // return newObstaclePositions.length
 };
 
 const expectedSecondSolution = 6;
 
 export { part1, expectedFirstSolution, part2, expectedSecondSolution };
+
+        // // check if there is a super easy loop to detect        
+        // collisions.forEach(({obstruction, collisionHeading, collisionIndex}) => {
+        //     // ignore future collisions, as may not cause a loop
+        //     if (collisionIndex > positionIndex) {
+        //         return;
+        //     }
+            
+        //     // ignore collisions with an inappropriate heading
+        //     if (!areEqual(collisionHeading, rightHeading)) {
+        //         return;
+        //     }
+            
+        //     // we cannot place an obstacle where the guard starts
+        //     if(areEqual(newPosition, startPosition)) {
+        //         return;
+        //     }
+
+        //     // ignore if we are going North, and the obstruction is not West of us
+        //     if (areEqual(heading, North) && isWestOf(obstruction, position)) return;
+
+        //     // ignore if we are going South, and the obstruction is not East of us
+        //     if (areEqual(heading, South) && isEastOf(obstruction, position)) return;
+
+        //     // ignore if we are going East, and the obstruction is not South of us
+        //     if (areEqual(heading, East) && isSouthOf(obstruction, position)) return;
+            
+        //     // ignore if we are going West, and the obstruction is not North of us
+        //     if (areEqual(heading, West) && isNorthOf(obstruction, position)) return;
+
+        //     newObstaclePositions.push([newPosition, heading])
+        // })
